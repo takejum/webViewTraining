7dea781753162a69945475a23951ecd20a48b7eb
Jumpei Takeshita <takejum.123@gmail.com>
2020-03-15 15:42:15 +0900

    Initial Commit

---
+++ webViewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  webViewApp
+//
+//  Created by Jumpei Takeshita on 2020/03/15.
+//  Copyright © 2020 Jumpei Takeshita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
